40ee8968559498e895589b24d41507374158be54
Laleet Avaiya <lmavaiya@gmail.com>
2022-08-13 14:07:53 +0530

    Domail specific email added

--- src/layout/PageLayout.js
+++ src/layout/PageLayout.js
@@ -158,7 +158,7 @@ const PageLayout = () => {
 
             <Row>
               <Col sm={8} md={6} lg={4}>
-                <a href="mailto: tanushsoftware@gmail.com">
+                <a href="mailto: info@tanushsoftware.com">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
