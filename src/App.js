78dd6ff8f8f8940305fce2e9c3f9b43ea4a758ff
Laleet Avaiya <lmavaiya@gmail.com>
2022-12-20 17:40:07 +0530

    Removed cool text

--- src/App.js
+++ src/App.js
@@ -16,20 +16,5 @@ function App() {
   );
 }
 
-// function ProdApp() {
-//   return (
-//     <div>
-//       <div style={{ textAlign: "center", fontSize: 25, position: "absolute", top: "50%", left: "50%", transform: "translate(-50%,-50%)", color: "green", padding: 100, }}>
-//         <h1>🆆🅴🅻🅲🅾🅼🅴</h1>
-//         <h2>🆃🅾 </h2>
-//         <h1>🆃🅰🅽🆄🆂🅷 🆂🅾🅵🆃🆆🅰🆁🅴</h1>
-//       </div>
-
-//       <a href="mailto: tanushsoftware87@gmail.com" style={{ textAlign: "center", transform: "translateX(-50%)", fontSize: 22, color: "black", position: "absolute", bottom: 80, left: "50%", fontStyle:"italic", fontFamily: "monospace" }}>
-//         Connect
-//         </a>
-//     </div>
-//   )
-// }
 
 export default App;
