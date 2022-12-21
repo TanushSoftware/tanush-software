230fd8bbd8bd04e361a5dc59545ae912b304af17
Laleet Avaiya <lmavaiya@gmail.com>
2022-12-21 18:19:23 +0530

    whats-today-privacypolicy added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import PageLayout from "./layout/PageLayout";
 import PrivacyPolicy from "./components/PrivacyPolicy";
 import PrivacyPolicyCheckBlindness from "./components/PrivacyPolicyCheckBlindness";
+import PrivacyPolicyWhatsToday from "./components/PrivacyPolicyWhatsToday";
 import PrivacyPolicyDailyDeals from "./components/PrivacyPolicyDailyDeals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
