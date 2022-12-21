5ec2fca1a5c9ad9da4c3253e94abaae4b609a330
Laleet Avaiya <lmavaiya@gmail.com>
2022-12-21 13:27:21 +0530

    policy updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import PageLayout from "./layout/PageLayout";
 import PrivacyPolicy from "./components/PrivacyPolicy";
+import PrivacyPolicyCheckBlindness from "./components/PrivacyPolicyCheckBlindness";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<PageLayout></PageLayout>}></Route>
         <Route path="privacypolicy" element={<PrivacyPolicy />} />
-        <Route path="check-color-blindness-privacypolicy" element={<PrivacyPolicy />} />
+        <Route path="check-color-blindness-privacypolicy" element={<PrivacyPolicyCheckBlindness />} />
       </Routes>
     </BrowserRouter>
   );
